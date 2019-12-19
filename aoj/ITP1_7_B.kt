f12ccef863c6bdf9caf7ed96d0459851f794c46e
com4dc <=>
2019-12-19 18:12:30 +0900

    - Add ITP1_7_B

--- aoj/ITP1_7_B.kt
+++ aoj/ITP1_7_B.kt
@@ -2,4 +2,30 @@ import java.util.Scanner
 
 fun main(args: Array<String>) {
     val scan = Scanner(System.`in`)
+
+    while(true) {
+        val nums = scan.nextLine().split(" ")
+        val n = Integer.parseInt(nums[0])
+        val x = Integer.parseInt(nums[1])
+
+        if (n == 0 && x == 0) {
+            break
+        }
+
+        println(detect(n, x))
+    }
+}
+
+fun detect(n: Int, x: Int): Int {
+    var count = 0
+    for (i in 1..n) {
+        for (j in 1..n) {
+            for (k in 1..n) {
+                if (i != j && j != k && i != k && i + j + k == x && i < j && j < k) {
+                    count++
+                }
+            }
+        }
+    }
+    return count
 }
