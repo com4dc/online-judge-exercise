5663dfe68ea149b39569a7bfd43a34736ec504dd
com4dc <=>
2020-06-25 17:21:30 +0900

    - feat: コードを見て理解した箇所をコメントした

--- aoj/ALDS1_4_B_solved.go
+++ aoj/ALDS1_4_B_solved.go
@@ -17,22 +17,27 @@ func nextInt() int64 {
 
 func binarySearch(x, n int64, arr []int64) bool {
 
+	// 内部で回す関数を定義
 	var reSearch func(b, e int64) bool
-
 	reSearch = func(b, e int64) bool {
+		// startとendのindexが合致、または逆転してれば評価終了。該当なし
 		if b >= e {
 			return false
 		}
+		// startとendの中間点を計算
 		m := (b + e) / 2
+		// 合致すれば true
 		if arr[m] == x {
 			return true
 		}
+		// 対象よりも低い数ならmの次のindexからendまでで再計算
 		if arr[m] < x {
 			return reSearch(m+1, e)
 		}
+		// 対象よりも大きな数ならbからmまでで再計算
 		return reSearch(b, m)
 	}
-
+	// 開始は0から最大値indexまで
 	return reSearch(0, n)
 }
 
@@ -49,19 +54,22 @@ func parseArrInt(l int64, strArr []string) []int64 {
 
 func main() {
 	sc.Split(bufio.ScanWords)
+	// 比較元の Array を構成
 	n := nextInt()
 	sarr := make([]int64, n)
 	for i, _ := range sarr {
 		sarr[i] = nextInt()
 	}
 
+	// 比較対象の Array を構成
 	q := nextInt()
 	tarr := make([]int64, q)
 	for i, _ := range tarr {
 		tarr[i] = nextInt()
 	}
 
-	count := 0
+	// 比較対象を回しながらバイナリサーチ
+	count := 0 // true の場合インクリメント
 	for _, t := range tarr {
 		if binarySearch(t, n, sarr) {
 			count++
